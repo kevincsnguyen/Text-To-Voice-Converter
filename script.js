e13c5c51c0b14fd52150c393a1994045da308a0b
kevincsnguyen <kevin.cs.nguyen@gmail.com>
2025-02-09 00:11:20 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let speech = new SpeechSynthesisUtterance();
+
+let voices = [];
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis.getVoices();
+  speech.voice = voices[0]; //speak first voice by default on device
+  voices.forEach(
+    (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
+  );
+};
+
+//change eventListner on each voice in dropdown
+voiceSelect.addEventListener("change", () => {
+  speech.voice = voices[voiceSelect.value];
+});
+
+document.querySelector("button").addEventListener("click", () => {
+  speech.text = document.querySelector("textarea").value;
+  window.speechSynthesis.speak(speech);
+});
